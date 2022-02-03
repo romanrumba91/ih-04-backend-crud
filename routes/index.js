8e7b992ad8281839e4c5531d48af30254ee24e56
Roman <romandiazloz91@gmail.com>
2022-02-03 12:14:32 -0600

    desarrollo de seeds

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@
 // 1. IMPORTACIONES
 const express		= require("express")
 const router		= express.Router()
-
+//Patron modular
+const indexController = require ("./../controllers/indexController")
 
 // 2. RUTEO
-router.get("/", (req, res) => {
-	res.send("hola")
-})
+router.get("/", indexController.getHome)
 
 
 
